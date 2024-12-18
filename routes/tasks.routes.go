d0eccf9196a77e49335359da5781ab9afe65ac8f
f-antonelli <antonelli.fd@gmail.com>
2024-12-17 21:06:06 -0300

    crud gorm postgres

---
+++ routes/tasks.routes.go
@@ -0,0 +1,69 @@
+package routes
+
+import (
+	"encoding/json"
+	"net/http"
+
+	"github.com/f-antonelli/gorm-restapi/db"
+	"github.com/f-antonelli/gorm-restapi/models"
+	"github.com/gorilla/mux"
+)
+
+func GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	var tasks []models.Task
+
+	db.DB.Find(&tasks)
+
+	json.NewEncoder(w).Encode(&tasks)
+}
+
+func GetTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+
+	params := mux.Vars(r)
+
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNotFound)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func CreateTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+
+	json.NewDecoder(r.Body).Decode(&task)
+
+	createdTask := db.DB.Create(&task)
+	err := createdTask.Error
+
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte(err.Error()))
+	}
+
+	json.NewEncoder(w).Encode(&task)
+}
+
+func DeleteTaskHandler(w http.ResponseWriter, r *http.Request) {
+	var task models.Task
+
+	params := mux.Vars(r)
+
+	db.DB.First(&task, params["id"])
+
+	if task.ID == 0 {
+		w.WriteHeader(http.StatusNoContent)
+		w.Write([]byte("Task not found"))
+		return
+	}
+
+	//db.DB.Delete(&task)
+	db.DB.Unscoped().Delete(&task)
+
+	w.WriteHeader(http.StatusOK)
+}
